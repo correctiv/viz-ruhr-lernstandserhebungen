601904e6abfbff2c80ff641b2b477bf784d619e7
Simon Wörpel <simon.woerpel@medienrevolte.de>
2017-04-27 11:53:01 +0200

    Maps: Disable mouseout events.

--- src/scripts/multi_maps.js
+++ src/scripts/multi_maps.js
@@ -38,6 +38,7 @@ export default () => {
         yExtent,
         getId: f => f.properties.RS,
         responsiveSvg: true,
+        mouseout: false
         // projection: d3.geoEquirectangular()
       })
 
